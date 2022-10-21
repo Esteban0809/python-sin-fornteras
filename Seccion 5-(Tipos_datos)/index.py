64b0ba9843161820b3468409fab00b40995b517d
Esteban0809 <106866914+Esteban0809@users.noreply.github.com>
2022-10-21 23:09:11 +0200

    actualizado el 21/10/22
    archivos y carpetas del curso python sin fronteras de las secciones que he ido terminando

---
+++ Seccion 5-(Tipos_datos)/index.py
@@ -0,0 +1,175 @@
+# Strings #
+
+# Se puede usar tanto las comillas simples como las dobles
+
+palabra = 'hola mundo' # string
+oracion = 'hola mundo comilla doble'
+
+entero = 20 # integer
+con_decimales = 20.2 # float
+complejo = 1j # es necesario utilizar una j al final de una variable de numero complejo
+
+#print (palabra, oracion, entero, con_decimales, complejo)
+
+# Listas #
+
+#lista = [1, 2, 3] 
+lista = ['Hola', 'Mundo', 'Chanchito feliz']
+
+# Al imprimir una lista vacia solo se muestran los corchetes: []
+
+# Para copiar una lista a otra:
+lista2 = lista.copy()
+
+# Para agregar un elemento a la lista:
+#lista.append(4)
+lista.append('Chanchito triste')
+
+# Para eliminar todos los elementos de la lista:
+#lista.clear()
+
+#print(lista, lista2)
+
+# Para contar la cantidad de repeticiones de un elemento de la lista:
+#print(lista2.count(3))
+
+# Para contar los elementos de la lista:
+#print(len(lista2))
+largo_lista = len(lista)
+#print(largo_lista)
+
+# Para acceder a los elementos de la lista:
+# print(lista[0])
+# Siempre se utilizan indices y no la posición del elemento
+
+# Para borrar el último elemento de la lista:
+# lista.pop()
+# print(lista)
+
+# Para eliminar un elemento en concreto
+# lista.remove('Hola')
+# print(lista)
+
+# Para revertir el orden de la lista:
+# lista.reverse()
+# print(lista)
+
+# Para ordenar la lista en orden ascendente:
+lista.sort()
+# print(lista)
+
+# Tuplas #
+
+# A diferencia de las listas, no puedes modificar a las tuplas
+
+tupla = ('Hola','mundo','somos','tupla')
+# print(tupla)
+
+# Para contar la cantidad de repeticiones de un elemento de la tupla:
+# print(tupla.count('mundo'))
+
+# # Para devolver el índice del elemento:
+# print(tupla.index('mundo'))
+
+# Como las tuplas no se pueden modificar se deben de castear a una lista
+# lista_de_tupla = list(tupla)
+# lista_de_tupla.append('Chanchito')
+# print(lista_de_tupla)
+
+# Rangos #
+
+rango = range(6)
+# print(rango)
+
+# Diccionarios #
+
+diccionario =  {
+        "nombre" : "Mishifu",
+        "raza" : "Persa",
+        "edad" : 5
+        }
+
+# print(diccionario)
+# Para acceder a un valor se debe digitar la llave entre corchetes:
+# print(diccionario['nombre'])
+# Sin embargo un método para acceder a los valores es:
+# print(diccionario.get('nombre'))
+
+# Para modificar algún valor:
+diccionario['nombre'] = 'MishiGod'
+# print(diccionario)
+
+# Para saber la cantidad de elementos:
+# print(len(diccionario))
+
+# Para agregar una llave con su valor:
+diccionario['ronronea'] = 'Si'
+# print(diccionario)
+
+# Para eliminar una propiedad(llave y valor):
+# diccionario.pop('raza')
+# o 
+# del diccionario['ronronea']
+
+# Para eliminar la última propiedad ingresada al diccionario:
+# diccionario.popitem()
+
+# Para copiar el diccionario:
+# copia_gato = diccionario.copy()
+# Otra forma:
+copia_gato = dict(diccionario)
+del diccionario['ronronea']
+
+# Para limpiar el diccionario:
+diccionario.clear()
+
+# print(diccionario, copia_gato)
+
+# Diccionarios anidados
+# Primer forma:
+
+# gatitos = {
+#         "Fluffy" : {
+#             "nombre" : "Fluffy",
+#             "edad" : 4,
+#             },
+#         "Mamba" : {
+#             "nombre" : "Black Mamba",
+#             "edad" : 12,
+#             }
+#         }
+
+# Segunda forma:
+
+fluffy = {
+        "nombre" : "Fluffy",
+        "edad" : 4
+        }
+
+mamba = {
+        "nombre" : "Black Mamba",
+        "edad" : 12
+        }
+
+gatitos = {
+        "Fluffy" : fluffy,
+        "Mamba" : mamba
+        }
+
+# print(gatitos)
+
+# Constructor dict
+
+perritos = dict(nombre = "Chanchito feliz", edad = 5)
+print(perritos)
+
+# Ambas sintaxis para crear diccionarios son válidas y ninguna es mejor que la otra
+
+# Booleanos #
+
+# La primera letra debe ir con mayúscula
+
+verdadero = True
+falso = False
+
+print(verdadero, falso)
---
+++ Seccion 6-(control_flujo)/index.py
@@ -0,0 +1,71 @@
+# Condicionales
+
+# if 2 < 5:
+#     print("2 es menor que 5")
+
+# Comparación de igualdad ==
+# Comparación de diferencia !=
+# Comparación menor <
+# Comparación mayor >
+# Comparación menor igual <=
+# Comparación mayor igual >=
+
+# if 2 == 2:
+#     print('2 es igual a 2')
+    
+# if 2 == 3:
+#     print('2 es igual a 3')
+
+# if 2 > 5: 
+#     print('2 es mayor a 5')
+
+# if 5 > 2: 
+#     print('5 es mayor a 2')
+
+# if 2 != 2:
+#     print('2 es distinto a 2')
+
+# if 3 != 2:
+#     print('3 es distinto a 2')
+
+# if 3 >= 2:
+#     print('3 es mayor o igual a 2')
+
+# if 3 >= 3:
+#     print('3 es mayor o igual a 3')
+
+
+# Elif, Else #
+
+if 2 > 5:
+    print('2 menor a 5 en if')
+elif 2 > 5:
+    print('2 menor a 5 en elif')
+elif 2 < 5:
+    print('2 menor a 5 en segundo elif')
+else:
+    print('yo me imprimo solo si todo lo anterior es falso')
+
+# No es obligatorio que exista un elif entre el if y el else
+
+# If cortos #
+
+if 2 < 5: print('If de una linea')
+
+# Operador ternario #
+
+# Operacion en caso verdadero IF condicion ELSE Operacion en caso falso
+
+print('Cuando devuelve true') if 5 > 2 else print('Cuando devuelve false')
+
+# And y or #
+
+# And requiere que ambas condiciones sean verdaderas para retornar verdadero
+
+if 2 < 5 and 3 > 2:
+    print('ambas devuelven true')
+
+# Or requiere que al menos una de las condiciones sea verdadera para retornar verdadero
+
+if 2 > 5 or 3 > 2:
+    print('devolvera true')
